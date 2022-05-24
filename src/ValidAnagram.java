a9f6d48efeef47a92835606eb7173c9a851905fe
podchez <102527343+podchez@users.noreply.github.com>
2022-05-24 13:29:51 +0300

    Update ValidAnagram.java

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -1,11 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (t.length() != s.length())
+        if (s.length() != t.length())
             return false;
         
-        List<Integer> sIntListSorted = s.chars().sorted().boxed().collect(Collectors.toList());
-        List<Integer> tIntListSorted = t.chars().sorted().boxed().collect(Collectors.toList());
+        char[] sCharArray = s.toCharArray();
+        char[] tCharArray = t.toCharArray();
         
-        return sIntListSorted.equals(tIntListSorted);
+        Arrays.sort(sCharArray);
+        Arrays.sort(tCharArray);
+        
+        return Arrays.equals(sCharArray, tCharArray);
+        
+        // old solution (lists and streams instead of arrays)
+//         if (t.length() != s.length())
+//             return false;
+        
+//         List<Integer> sIntListSorted = s.chars().sorted().boxed().collect(Collectors.toList());
+//         List<Integer> tIntListSorted = t.chars().sorted().boxed().collect(Collectors.toList());
+        
+//         return sIntListSorted.equals(tIntListSorted);
     }
 }
